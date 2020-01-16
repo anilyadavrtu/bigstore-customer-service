99ccc4ca7ab01609ac1d12ed847eb630851f7f3e
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 17:45:41 +0530

    base project setup

---
+++ src/main/java/com/bigstore/bigstorecustomerservice/BigstoreCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstorecustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigstoreCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigstoreCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigstore/bigstorecustomerservice/BigstoreCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstorecustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigstoreCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
